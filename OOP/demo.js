5136543e27752d2bf8d127d789488a30f662b73b
quangnamhd9x <quangnamhd9x@gmail.com>
2020-07-25 09:46:50 +0700

    update

---
+++ OOP/demo.js
@@ -0,0 +1,31 @@
+class myDate{
+    constructor(day,month,year) {
+        this._day = day;
+        this._month = month;
+        this._year = year;
+    }
+
+    get day() {
+        return this._day;
+    }
+
+    set day(value) {
+        this._day = value;
+    }
+
+    get month() {
+        return this._month;
+    }
+
+    set month(value) {
+        this._month = value;
+    }
+
+    get year() {
+        return this._year;
+    }
+
+    set year(value) {
+        this._year = value;
+    }
+}
