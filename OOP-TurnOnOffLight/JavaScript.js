0f0ef8f7d8b14f026ee3bee6c42f33a66e56ec05
quangnamhd9x <quangnamhd9x@gmail.com>
2020-07-27 22:54:45 +0700

    update

---
+++ OOP-TurnOnOffLight/JavaScript.js
@@ -0,0 +1,18 @@
+class SwitchButton {
+    constructor(status, lamp) {
+        this.status = true;
+        this.lamp = ElectricLamp;
+    }
+    SwitchBunton(){
+
+    }
+    connectToLamp(ElectricLamp){
+        this.E
+    }
+    switchOff(){
+
+    }
+    switchOn(){
+        
+    }
+}
