9d54e7c76b0f285988f46d4a95542902ef364de1
unknown <quangnamhd9x@gmail.com>
2020-09-19 23:30:34 +0700

    update

--- C-to-F/c-to-f.js
+++
@@ -1,8 +0,0 @@
-let inputcelsius;
-
-inputcelsius = prompt("nhap do C:");
-
-let c = parseInt(inputcelsius);
-let doF = (c*2)+30;
-
-alert("do F="+ doF);
---
+++ [Thực hành] Chuyển đổi nhiệt độAssignment/c-to-f.js
@@ -0,0 +1,8 @@
+let inputcelsius;
+
+inputcelsius = prompt("nhap do C:");
+
+let c = parseInt(inputcelsius);
+let doF = (c * 2) + 30;
+
+alert("do F=" + doF);
